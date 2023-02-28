9661f46d4c5879f5576449e3d992eed6682d329b
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-27 19:39:50 -0500

    render function refactored

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-document.getElementById('hero').innerHTML = 
-`<div class="character-card">
-    <h4 class="name"> Wizard </h4>
-    <img class="avatar" src="images/wizard.png"/>
-    <p class="health">health: <b> 60 </b></p>
-    <div class="dice-container"><div class="dice"> 6 </div></div>
- </div> `
+
 
 document.getElementById('monster').innerHTML = renderCharacter(elementId, name, avatar, health, diceScore)
 `<div class="character-card">
@@ -14,13 +8,25 @@ document.getElementById('monster').innerHTML = renderCharacter(elementId, name, 
     <div class="dice-container"><div class="dice"> 4 </div></div>
  </div>`
  
- const elementId = `<div class="character-card"> </div>`
- const name = `<h4 class="name"> Orc </h4>`
- const avatar = ` <img class="avatar" src="images/orc.png"/>`
- const health = `<p class="health">health: <b> 60 </b></p>`
- const diceScore = `<div class="dice-container"><div class="dice"> 4 </div></div`
+ const heroelementId = "hiro"
+ const heroname = "Wizrd"
+ const heroavatar = "images/wizard.png" 
+ const herohealth =  "60"
+ const herodiceScore = 6
+ 
+ const monsterelementId = "monster"
+ const monstername = "Orc"
+ const monsteravatar = "images/orc.png" 
+ const monsterhealth =  "10"
+ const monsterDiceScore = 4
 
  function renderCharacter(elementId, name, avatar, health, diceScore){
+    document.getElementById(elementId).innerHTML = `<div class="character-card">
+    <h4 class="name">${name}</h4>
+    <img class="avatar" src="${avatar}"/>
+    <p class="health">health: <b> ${health}</b></p>
+    <div class="dice-container"><div class="dice">${diceScore}</div></div>
+ </div> `
 
  }
- renderCharacter()
+ renderCharacter(heroelementId, heroname, heroavatar, herohealth, herodiceScore)
