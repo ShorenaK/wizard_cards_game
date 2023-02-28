3f85a6da050d2bc3ef2bbf51bb935ddcd4f81647
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-27 20:39:57 -0500

    object destructering

--- index.js
+++ index.js
@@ -16,14 +16,18 @@ const monster = {
     health : "10", 
     diceRoll : 4
 }
+
+
+
 // function to render character cards
 function renderCharacter(data){
-  document.getElementById(data.elementId).innerHTML = 
+const {elementId, name, avatar, health, diceRoll }= data
+  document.getElementById(elementId).innerHTML = 
     `<div class="character-card">
-        <h4 class="name"> ${data.name} </h4>
-        <img class="avatar" src="${data.avatar}"/>
-        <p class="health">health: <b> ${data.health}</b></p>
-        <div class="dice-container"><div class="dice"> ${data.diceRoll} </div></div>
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}"/>
+        <p class="health">health: <b> ${health}</b></p>
+        <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
     </div>`;   
 }
 // render hero and monster character cards
