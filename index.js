b101b95219b08205db4ad5f5c81b194febf11e0e
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-27 19:30:22 -0500

    render function created

--- index.js
+++ index.js
@@ -6,10 +6,21 @@ document.getElementById('hero').innerHTML =
     <div class="dice-container"><div class="dice"> 6 </div></div>
  </div> `
 
-document.getElementById('monster').innerHTML =
+document.getElementById('monster').innerHTML = renderCharacter(elementId, name, avatar, health, diceScore)
 `<div class="character-card">
     <h4 class="name"> Orc </h4>
     <img class="avatar" src="images/orc.png"/>
     <p class="health">health: <b> 10 </b></p>
     <div class="dice-container"><div class="dice"> 4 </div></div>
  </div>`
+ 
+ const elementId = `<div class="character-card"> </div>`
+ const name = `<h4 class="name"> Orc </h4>`
+ const avatar = ` <img class="avatar" src="images/orc.png"/>`
+ const health = `<p class="health">health: <b> 60 </b></p>`
+ const diceScore = `<div class="dice-container"><div class="dice"> 4 </div></div`
+
+ function renderCharacter(elementId, name, avatar, health, diceScore){
+
+ }
+ renderCharacter()
