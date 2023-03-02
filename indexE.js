20b29a51ac2d7d4a20f7bd0089076db8071858cc
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-02 09:42:13 -0500

    some code lines transfared to indexE.js file

--- indexE.js
+++ indexE.js
@@ -42,3 +42,80 @@
 
 // constructor class 
 
+
+const animalForRlease1 = {
+    name: 'Tilly', 
+    species: 'tiger', 
+    weghtKg: 56, 
+    age: 2, 
+    dataOfRelease: '03-02-2022'
+}
+const animalForRlease2 = {
+    name: 'Nelly', 
+    species: 'elephant', 
+    weghtKg: 320, 
+    age: 16, 
+    dataOfRelease: '03-02-2022'
+}
+function Animal(data){
+    this.name = data.name
+    this.species = data.species
+    this.weightKg = data.weghtKg
+    this.age = data.age
+    this.dataOfRelease = data.dataOfRelease
+    this.summeriseAnimal = function(){
+        console.log(`${this.name} is a ${this.age} 
+        year old ${this.species} which weighs 
+        ${this.weghtKg} release into the wild on
+        ${this.dataOfRelease}`)
+    }
+}
+const tillyThetiger = new Animal(animalForRlease1)
+const nellyTheElephant = new Animal(animalForRlease2)
+
+console.log(tillyThetiger)
+console.log(nellyTheElephant)
+tillyThetiger.summeriseAnimal()
+nellyTheElephant.summeriseAnimal()
+
+const hotel1 = {
+	name: 'Safari View',
+	rooms: 30,
+	stars: '⭐⭐⭐⭐⭐',
+	costPerNightAdult: 240,
+    costPerNightChild: 180
+}
+const hotel2 = {
+	name: 'Leopard Mansion',
+	rooms: 96,
+	stars: '⭐⭐⭐',
+	costPerNightAdult: 120,
+    costPerNightChild: 180
+}
+function NationalParkHotels(data){
+    this.name = data.name
+    this.rooms = data.rooms
+    this.starts = data.stars
+    this.costPerNightAdult = data.costPerNightAdult  
+    this.costPerNightChild = data.costPerNightChild
+    this.totalPrice = data.costPerNightAdult + data.costPerNightChild
+    this.summeriseHotel = function(){
+        const totalPrice = this.costPerNightAdult * 2 + this.costPerNightChild * 2
+        console.log(` 
+        A one night stay at the ${this.name}
+        for two children is ${this.costPerNightChild}
+        for two adult is ${this.costPerNightAdult} 
+        total cost of ${totalPrice}
+        `)
+    }
+}
+// creating instance of NarionalParkHotles for each o
+// object (hotle )
+const hotel1class = new NationalParkHotels(hotel1)
+const hotel2class = new NationalParkHotels(hotel2)
+
+hotel1class.summeriseHotel()
+hotel2class.summeriseHotel()
+
+// console.log(hotel1)
+// console.log(hotel2)
