53b683134236c0951a5ecae208fa1c57b0b1069c
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-02 13:22:34 -0500

    uncommented destructuring

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ function Character(data){
         }).join('')
      }
     this.getCharacterHtml = function(){
-// const { elementId, name, avatar, health, diceCount } = this; 
+ const { elementId, name, avatar, health, diceCount } = this; 
 // we can use diceCount, elementId, name, avatar, health without dadta if we use distructering 
-    const diceHtml = this.getDiceHtml(data.diceCount)
+    const diceHtml = this.getDiceHtml(diceCount)
     return `<div class="character-card">
-        <h4 class="name"> ${data.name} </h4>
-        <img class="avatar" src="${data.avatar}" />
-        <div class="health">health: <b> ${data.health} </b></div>
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}" />
+        <div class="health">health: <b> ${health} </b></div>
         <div class="dice-container">
             ${diceHtml}
         </div>
